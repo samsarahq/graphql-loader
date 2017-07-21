54a70859bb0f2e486c9efdd2aef8d46b66734589
Stephen Wan <stephen@stephenwan.com>
2017-07-20 21:32:13 -0700

    test: add snapshot tests

--- test/base.spec.ts
+++ test/base.spec.ts
@@ -1,24 +1,28 @@
 import { compile } from "./runner";
+import * as snapshot from "snap-shot";
 
 describe("loader", function() {
   it("should load a simple query", async function() {
-    await compile({
+    const document = await compile({
       context: __dirname + "/fixtures/simple",
       entry: "./query.graphql",
     });
+    snapshot(document);
   });
 
   it("should load an imported fragment", async function() {
-    await compile({
+    const document = await compile({
       context: __dirname + "/fixtures/fragments",
       entry: "./query.graphql",
     });
+    snapshot(document);
   });
 
   it("should ignore duplicate an imported fragment", async function() {
-    await compile({
+    const document = await compile({
       context: __dirname + "/fixtures/fragments-common-duplicates",
       entry: "./query.graphql",
     });
+    snapshot(document);
   });
 });
