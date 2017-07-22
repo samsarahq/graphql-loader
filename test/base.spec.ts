7b63f03ea8ab2e07f28c2121b68748befa168f4d
Stephen Wan <stephen@stephenwan.com>
2017-07-21 19:04:48 -0700

    test: make spec cute (fully parameterized)

--- test/base.spec.ts
+++ test/base.spec.ts
@@ -1,47 +1,27 @@
 import { runFixture, WebpackError } from "./runner";
 
 describe("graphql-loader", function() {
-  it("should load a simple query", async function() {
-    const document = await runFixture("simple");
-    expect(document).toMatchSnapshot();
-  });
-
-  it("should load an imported fragment", async function() {
-    const document = await runFixture("fragments");
-    expect(document).toMatchSnapshot();
-  });
-
-  it("should ignore duplicate an imported fragment", async function() {
-    const document = await runFixture("fragments-common-duplicates");
-    expect(document).toMatchSnapshot();
-  });
-
-  it("validates graphql query", async function() {
-    const document = await runFixture("validator");
-    expect(document).toMatchSnapshot();
-  });
-
-  it("fails to parse invalid document", async function() {
-    try {
-      const document = await runFixture("fail-invalid-document");
-    } catch (err) {
-      expect(err).toMatchSnapshot();
-    }
-  });
-
-  it("fails to validate invalid field", async function() {
-    try {
-      const document = await runFixture("fail-invalid-field");
-    } catch (err) {
-      expect(err).toMatchSnapshot();
-    }
-  });
-
-  it("fails to validate missing fragment", async function() {
-    try {
-      const document = await runFixture("fail-missing-fragment");
-    } catch (err) {
-      expect(err).toMatchSnapshot();
-    }
-  });
+  Object.entries({
+    "should load a simple query": "simple",
+    "should load an imported fragment": "fragments",
+    "should ignore duplicate an imported fragment":
+      "fragments-common-duplicates",
+    "validates graphql query": "validator",
+    "fails to parse invalid document": "fail-invalid-document",
+    "fails to validate invalid field": "fail-invalid-field",
+    "fails to validate missing fragment": "fail-missing-fragment",
+  }).forEach(([name, fixturePath]) =>
+    it(name, async function() {
+      try {
+        const document = await runFixture(fixturePath);
+        expect(document).toMatchSnapshot();
+      } catch (err) {
+        if (err instanceof WebpackError) {
+          expect(err).toMatchSnapshot();
+        } else {
+          throw err;
+        }
+      }
+    }),
+  );
 });
